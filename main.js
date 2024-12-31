87b0546a3c04a8353ebc429c981281caef232122
Stoo <stoo.goff@gmail.com>
2024-12-31 14:14:33 +0000

    And main

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 
-export { CollectionStore, ObjectStore } from '/store/index.js'
-export { Component } from '/reactive/component.js'
-export { directives } from '/reactive/directives.js'
-export { Emitter } from '/reactive/emitter.js'
+export { CollectionStore, ObjectStore } from './store/index.js'
+export { Component } from './reactive/component.js'
+export { directives } from './reactive/directives.js'
+export { Emitter } from './reactive/emitter.js'
