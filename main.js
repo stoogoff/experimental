066476baa69e99b2c60c1fc1ea354378796888cb
Stoo Goff <sgoff@powerx.ai>
2025-12-03 12:20:48 +0000

    Removed unneeded stuff and updated package scripts

--- main.js
+++
@@ -1,5 +0,0 @@
-
-export { CollectionStore, ObjectStore } from './store/index.js'
-export { Component } from './reactive/component.js'
-export { directives } from './reactive/directives.js'
-export { Emitter } from './reactive/emitter.js'
