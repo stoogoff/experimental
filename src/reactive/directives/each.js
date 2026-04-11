71cd1d13daedc4588f67057719e8e39387992cc8
Stoo <stoo.goff@gmail.com>
2026-04-12 00:13:03 +0100

    Adding removal of extra nodes and some code clean up

--- src/reactive/directives/each.js
+++ src/reactive/directives/each.js
@@ -1,16 +1,16 @@
 
+import { createId } from '../../utils/string.js'
+import { notNull } from '../../utils/assert.js'
 import { logger } from '../config.js'
 import { directives } from '../directives.js'
-import { createId } from '../../utils/string.js'
 
-const createNode = (context, base, value, index, ref) => {
-	logger().log(`each (directive): ${ ref } building index[${ index }]`, value, context)
+const createNode = (context, base, value, ref) => {
+	logger().log(`each (directive): ${ ref } building`, value, context)
 
 	const clone = base.cloneNode(true)
 
 	clone.setAttribute('data-current', JSON.stringify(value))
 	clone.setAttribute('data-ref', ref)
-	clone.setAttribute('data-index', index)
 
 	// create scope from value or by cloning from the component
 	const newScope = 'clone' in context.scope ? context.scope.clone(value) : value
@@ -27,15 +27,12 @@ export const each = (context) => {
 	const initialValues = context.value ?? []
 	const parent = context.node.parentNode
 	const base = context.node.cloneNode(true)
+	const insertionPoint = context.node.previousElementSibling
 
 	logger().log(`each (directive): ${ ref } building`, context, parent, initialValues)
 
-	const nodeList = []
-
 	// clone nodes
-	initialValues.forEach((value, index) => {
-		nodeList.push(createNode(context, base, value, index, ref))
-	})
+	const nodeList = initialValues.map(value => createNode(context, base, value, ref))
 
 	context.node.replaceWith(...nodeList)
 
@@ -44,7 +41,7 @@ export const each = (context) => {
 
 		// get a list of nodes which belong to this each
 		const affectedNodes = Array.from(parent.querySelectorAll(`*[data-ref=${ref}]`))
-		let lastNode = null
+		let lastNode = insertionPoint
 
 		values.forEach((value, index) => {
 			// ignore a node if it already exists
@@ -58,7 +55,7 @@ export const each = (context) => {
 				}
 				else {
 					logger().log(`each (directive): ${ ref } rebuilding ${ index } — different value replacing`)
-					const clone = createNode(context, base, value, index, ref)
+					const clone = createNode(context, base, value, ref)
 
 					affectedNodes[index].replaceWith(clone)
 					lastNode = clone
@@ -66,13 +63,30 @@ export const each = (context) => {
 			}
 			else {
 				logger().log(`each (directive): ${ ref } rebuilding ${ index } — new value creating`)
-				const clone = createNode(context, base, value, index, ref)
 
-				// insert the clone after the last affected node
-				lastNode.insertAdjacentElement('afterend', clone)
+				const clone = createNode(context, base, value, ref)
+
+				if(notNull(lastNode)) {
+					// insert the clone after the last affected node if it exists
+					lastNode.insertAdjacentElement('afterend', clone)
+				}
+				else {
+					// otherwise append to the parent
+					parent.appendChild(clone)
+				}
+
 				lastNode = clone
 			}
 		})
+
+		// remove any additional nodes
+		if(values.length < affectedNodes.length) {
+			logger().log(`each (directive): ${ ref } rebuilding — removing ${ affectedNodes.length - values.length } node(s)`)
+
+			for(let i = affectedNodes.length - 1, len = values.length; i >= len; --i) {
+				affectedNodes[i].remove()
+			}
+		}
 	})
 
 	return true
